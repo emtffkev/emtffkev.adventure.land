0eab0f280bdd3bd19fe9cab98425cac4dd151130
Unknown <emtffkev@gmail.com>
2017-01-16 23:02:43 -0500

    Beautified

--- emtffkevlib.js
+++ emtffkevlib.js
@@ -1,146 +1,144 @@
 function find_item_filter(filter, search_slot) {
-  let slot = search_slot;
-  if (!slot)
-    slot = 0
+    let slot = search_slot;
+    if (!slot)
+        slot = 0
 
-  for (let i = slot; i < character.items.length; i++) {
-    let item = character.items[i];
+    for (let i = slot; i < character.items.length; i++) {
+        let item = character.items[i];
 
-    if (item && filter(item))
-      return [i, character.items[i]];
-  }
+        if (item && filter(item))
+            return [i, character.items[i]];
+    }
 
-  return [-1, null];
+    return [-1, null];
 }
 
 function purchase_potions(buyHP, buyMP) {
-  let [hpslot, hppot] = find_item_filter(i => i.name == hp_potion);
-  let [mpslot, mppot] = find_item_filter(i => i.name == mp_potion);
+    let [hpslot, hppot] = find_item_filter(i => i.name == hp_potion);
+    let [mpslot, mppot] = find_item_filter(i => i.name == mp_potion);
 
-  if (buyHP && (!hppot || hppot.q < pots_minimum)) {
-    parent.buy(hp_potion, pots_to_buy);
-    set_message("Buying HP pots.");
-  }
-  if (buyMP && (!mppot || mppot.q < pots_minimum)) {
-    parent.buy(mp_potion, pots_to_buy);
-    set_message("Buying MP pots.");
-  }
+    if (buyHP && (!hppot || hppot.q < pots_minimum)) {
+        parent.buy(hp_potion, pots_to_buy);
+        set_message("Buying HP pots.");
+    }
+    if (buyMP && (!mppot || mppot.q < pots_minimum)) {
+        parent.buy(mp_potion, pots_to_buy);
+        set_message("Buying MP pots.");
+    }
 }
 
-function loot()
-{
-	var looted=0;
-	if(safeties && mssince(last_loot)<200) return;
-	last_loot=new Date();
-	for(id in parent.chests)
-	{
-		var chest=parent.chests[id];
-		if(safeties && (chest.items>character.esize || chest.last_loot && mssince(chest.last_loot)<1600)) continue;
-		chest.last_loot=last_loot;
-		parent.socket.emit("open_chest",{id:id});
-		looted++;
-		//if(looted==2) break;
-	}
+function loot() {
+    var looted = 0;
+    if (safeties && mssince(last_loot) < 200) return;
+    last_loot = new Date();
+    for (id in parent.chests) {
+        var chest = parent.chests[id];
+        if (safeties && (chest.items > character.esize || chest.last_loot && mssince(chest.last_loot) < 1600)) continue;
+        chest.last_loot = last_loot;
+        parent.socket.emit("open_chest", {
+            id: id
+        });
+        looted++;
+        //if(looted==2) break;
+    }
 }
 
 // Add XP Timer
 
 function init_xptimer(minref) {
-  minute_refresh = minref || 1;
-  parent.add_log(minute_refresh.toString() + ' min until tracker refresh!', 0x00FFFF);
+    minute_refresh = minref || 1;
+    parent.add_log(minute_refresh.toString() + ' min until tracker refresh!', 0x00FFFF);
 
-  let $ = parent.$;
-  let brc = $('#bottomrightcorner');
+    let $ = parent.$;
+    let brc = $('#bottomrightcorner');
 
-  brc.find('#xptimer').remove();
+    brc.find('#xptimer').remove();
 
-  let xpt_container = $('<div id="xptimer"></div>').css({
-    background: 'black',
-    border: 'solid gray',
-    borderWidth: '5px 5px',
-    width: '320px',
-    height: '96px',
-    fontSize: '28px',
-    color: '#77EE77',
-    textAlign: 'center',
-    display: 'table',
-    overflow: 'hidden',
-    marginBottom: '-5px'
-  });
+    let xpt_container = $('<div id="xptimer"></div>').css({
+        background: 'black',
+        border: 'solid gray',
+        borderWidth: '5px 5px',
+        width: '320px',
+        height: '96px',
+        fontSize: '28px',
+        color: '#77EE77',
+        textAlign: 'center',
+        display: 'table',
+        overflow: 'hidden',
+        marginBottom: '-5px'
+    });
 
-  //vertical centering in css is fun
-  let xptimer = $('<div id="xptimercontent"></div>')
-    .css({
-      display: 'table-cell',
-      verticalAlign: 'middle'
-    })
-    .html('Estimated time until level up:<br><span id="xpcounter" style="font-size: 40px !important; line-height: 28px">Loading...</span><br><span id="xprate">(Kill something!)</span>')
-    .appendTo(xpt_container);
+    //vertical centering in css is fun
+    let xptimer = $('<div id="xptimercontent"></div>')
+        .css({
+            display: 'table-cell',
+            verticalAlign: 'middle'
+        })
+        .html('Estimated time until level up:<br><span id="xpcounter" style="font-size: 40px !important; line-height: 28px">Loading...</span><br><span id="xprate">(Kill something!)</span>')
+        .appendTo(xpt_container);
 
-  brc.children().first().after(xpt_container);
+    brc.children().first().after(xpt_container);
 }
 
 function update_xptimer() {
-  if (character.xp == last_xp_checked_kill) return;
+    if (character.xp == last_xp_checked_kill) return;
 
-  let $ = parent.$;
-  let now = new Date();
+    let $ = parent.$;
+    let now = new Date();
 
-  let time = Math.round((now.getTime() - last_minutes_checked.getTime()) / 1000);
-  if (time < 1) return; // 1s safe delay
-  let xp_rate = Math.round((character.xp - last_xp_checked_minutes) / time);
-  if (time > 60 * minute_refresh) {
-    last_minutes_checked = new Date();
-    last_xp_checked_minutes = character.xp;
-  }
-  last_xp_checked_kill = character.xp;
+    let time = Math.round((now.getTime() - last_minutes_checked.getTime()) / 1000);
+    if (time < 1) return; // 1s safe delay
+    let xp_rate = Math.round((character.xp - last_xp_checked_minutes) / time);
+    if (time > 60 * minute_refresh) {
+        last_minutes_checked = new Date();
+        last_xp_checked_minutes = character.xp;
+    }
+    last_xp_checked_kill = character.xp;
 
-  let xp_missing = parent.G.levels[character.level] - character.xp;
-  let seconds = Math.round(xp_missing / xp_rate);
-  let minutes = Math.round(seconds / 60);
-  let hours = Math.round(minutes / 60);
-  let counter = `${hours}h ${minutes % 60}min`;
+    let xp_missing = parent.G.levels[character.level] - character.xp;
+    let seconds = Math.round(xp_missing / xp_rate);
+    let minutes = Math.round(seconds / 60);
+    let hours = Math.round(minutes / 60);
+    let counter = `${hours}h ${minutes % 60}min`;
 
-  $('#xpcounter').text(counter);
-  $('#xprate').text(`${ncomma(xp_rate)} XP/s`);
+    $('#xpcounter').text(counter);
+    $('#xprate').text(`${ncomma(xp_rate)} XP/s`);
 }
 
 function ncomma(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 //function to find out how many chests are on the ground
-function getNumChests()
-{
+function getNumChests() {
     var count = 0;
-    for(id in parent.chests)
-    {
+    for (id in parent.chests) {
         count++;
     }
     return count;
 }
 
 // keepSafe - Keep at range during attack
-function keepSafe(){
-	var target = get_targeted_monster();
-	var maxRange = 70;
-	var minRange = 70;
+function keepSafe() {
+    var target = get_targeted_monster();
+    var maxRange = 70;
+    var minRange = 70;
 
-	var distance = Math.hypot(
-		character.real_x - target.real_x,
-		character.real_y - target.real_y
-	);
+    var distance = Math.hypot(
+        character.real_x - target.real_x,
+        character.real_y - target.real_y
+    );
 
-	// Comfortable range
-	if (distance > maxRange) {
-		move(
-			character.real_x + (target.real_x - character.real_x) / 2,
-			character.real_y + (target.real_y - character.real_y) / 2
-		);
-	} else if (distance < minRange) {
-		move(
-			character.real_x - (target.real_x - character.real_x) / 2,
-			character.real_y - (target.real_y - character.real_y) / 2
-		);
-	}
+    // Comfortable range
+    if (distance > maxRange) {
+        move(
+            character.real_x + (target.real_x - character.real_x) / 2,
+            character.real_y + (target.real_y - character.real_y) / 2
+        );
+    } else if (distance < minRange) {
+        move(
+            character.real_x - (target.real_x - character.real_x) / 2,
+            character.real_y - (target.real_y - character.real_y) / 2
+        );
+    }
 }
